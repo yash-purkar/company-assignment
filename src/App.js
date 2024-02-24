7e7bd0e48701fd0d268ac79bd787f6aa678e6a02
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-02-24 12:01:17 +0530

    feat: Displayed markers on the map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+
+// Markers on the map
+const markers = [
+  {
+    geocode: [48.86, 2.3522],
+    popUp: "Hello, I am pop up 1"
+  },
+  {
+    geocode: [48.85, 2.3522],
+    popUp: "Hello, I am pop up 2"
+  },
+  {
+    geocode: [48.855, 2.34],
+    popUp: "Hello, I am pop up 3"
+  }
+];
 
 function App() {
   return <MapContainer center={[48.8566,2.3522]} zoom={13}>
@@ -8,6 +24,9 @@ function App() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {
+        markers.map(marker => <Marker position={marker.geocode}></Marker>)
+      }
   </MapContainer>;
 }
 
