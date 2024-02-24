a6457562b5b89f870590591119171742919c13d4
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-02-24 13:59:20 +0530

    feat: blink dots if live traffic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,95 @@
 import { Icon } from "leaflet";
 import "./App.css";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  CircleMarker,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+} from "react-leaflet";
 import MarkerGroupCluster from "react-leaflet-cluster";
+import { useEffect, useState } from "react";
 // Markers on the map
 const markers = [
   {
-    geocode: [48.86, 2.3522],
+    latitude: 48.86,
+    longitude: 2.3522,
     popUp: "Hello, I am pop up 1",
+    isLiveTraffic: false,
   },
   {
-    geocode: [48.85, 2.3522],
+    latitude: 48.85,
+    longitude: 2.3522,
     popUp: "Hello, I am pop up 2",
+    isLiveTraffic: true,
   },
   {
-    geocode: [48.855, 2.34],
+    latitude: 48.855,
+    longitude: 2.34,
     popUp: "Hello, I am pop up 3",
+    isLiveTraffic: false,
+  },
+  {
+    latitude: 48.866,
+    longitude: 2.3622,
+    popUp: "Hello, I am pop up 3",
+    isLiveTraffic: true,
+  },
+  // ---
+  {
+    latitude: 48.846,
+    longitude: 2.342,
+    popUp: "Hello, I am pop up 3",
+    isLiveTraffic: false,
+  },
+  {
+    latitude: 48.861,
+    longitude: 2.332,
+    popUp: "Hello, I am pop up 3",
+    isLiveTraffic: true,
+  },
+  {
+    latitude: 48.836,
+    longitude: 2.372,
+    popUp: "Hello, I am pop up 3",
+    isLiveTraffic: false,
+  },
+  {
+    latitude: 48.871,
+    longitude: 2.367,
+    popUp: "Hello, I am pop up 3",
+    isLiveTraffic: true,
+  },
+  {
+    latitude: 48.841,
+    longitude: 2.337,
+    popUp: "Hello, I am pop up 3",
+    isLiveTraffic: false,
+  },
+  {
+    latitude: 48.881,
+    longitude: 2.332,
+    popUp: "Hello, I am pop up 3",
+    isLiveTraffic: true,
+  },
+  {
+    latitude: 48.846,
+    longitude: 2.377,
+    popUp: "Hello, I am pop up 3",
+    isLiveTraffic: false,
+  },
+  {
+    latitude: 48.876,
+    longitude: 2.341,
+    popUp: "Hello, I am pop up 3",
+    isLiveTraffic: true,
+  },
+  {
+    latitude: 48.841,
+    longitude: 2.362,
+    popUp: "Hello, I am pop up 3",
+    isLiveTraffic: false,
   },
 ];
 
@@ -25,22 +99,56 @@ const cusotomMarker = new Icon({
   iconSize: [38, 38],
 });
 
+const traffic = markers.reduce(
+  (acc, curr) => {
+    return curr.isLiveTraffic
+      ? { ...acc, liveTraffic: [...acc.liveTraffic, curr] }
+      : { ...acc, offline: [...acc.offline, curr] };
+  },
+  { liveTraffic: [], offline: [] }
+);
+
 function App() {
+  const [liveTraffic, setLiveTraffic] = useState(traffic.liveTraffic);
+  const [offline, setOffline] = useState(traffic.offline);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setLiveTraffic((prev) => (prev.length > 0 ? [] : traffic.liveTraffic));
+    }, 1000);
+
+    return () => clearTimeout(intervalId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [traffic.liveTraffic]);
+
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <MarkerGroupCluster>
-        {markers.map((marker, i) => (
-          <Marker key={i} position={marker.geocode} icon={cusotomMarker}>
-            <Popup>{marker.popUp}</Popup>
-          </Marker>
-        ))}
-      </MarkerGroupCluster>
-    </MapContainer>
+    <div className="marker-container">
+      <MapContainer center={[48.8566, 2.3522]} zoom={14}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <MarkerGroupCluster>
+          {[...offline, ...liveTraffic].map((location, i) => (
+            <CircleMarker
+              key={i}
+              center={[location.latitude, location.longitude]}
+              radius={8}
+              color="blue"
+            >
+              <Popup>{location.popUp}</Popup>
+            </CircleMarker>
+          ))}
+        </MarkerGroupCluster>
+      </MapContainer>
+    </div>
   );
 }
 
 export default App;
+
+// MarkerContainer - Container for the map
+// TileLayer - Skin of the map.
+// MarkerGroupCluster - Group for markers
+// Marker - Marker visible on the map
+// PopUp - Popup for the each marker
