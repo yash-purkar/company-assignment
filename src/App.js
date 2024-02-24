22bc270153cfb4984e0f4b2fa30e8299618e7d33
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-02-24 14:43:53 +0530

    feat: Tabs to navigate between map and sales

--- src/App.js
+++ src/App.js
@@ -1,146 +1,18 @@
-import { Icon } from "leaflet";
 import "./App.css";
 import "leaflet/dist/leaflet.css";
-import {
-  CircleMarker,
-  MapContainer,
-  Marker,
-  Popup,
-  TileLayer,
-} from "react-leaflet";
-import MarkerGroupCluster from "react-leaflet-cluster";
-import { useEffect, useState } from "react";
-// Markers on the map
-const markers = [
-  {
-    latitude: 48.86,
-    longitude: 2.3522,
-    popUp: "Hello, I am pop up 1",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 48.85,
-    longitude: 2.3522,
-    popUp: "Hello, I am pop up 2",
-    isLiveTraffic: true,
-  },
-  {
-    latitude: 48.855,
-    longitude: 2.34,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 48.866,
-    longitude: 2.3622,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: true,
-  },
-  // ---
-  {
-    latitude: 48.846,
-    longitude: 2.342,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 48.861,
-    longitude: 2.332,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: true,
-  },
-  {
-    latitude: 48.836,
-    longitude: 2.372,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 48.871,
-    longitude: 2.367,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: true,
-  },
-  {
-    latitude: 48.841,
-    longitude: 2.337,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 48.881,
-    longitude: 2.332,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: true,
-  },
-  {
-    latitude: 48.846,
-    longitude: 2.377,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 48.876,
-    longitude: 2.341,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: true,
-  },
-  {
-    latitude: 48.841,
-    longitude: 2.362,
-    popUp: "Hello, I am pop up 3",
-    isLiveTraffic: false,
-  },
-];
-
-// Creating icon for custom marker
-const cusotomMarker = new Icon({
-  iconUrl: require("./assets/marker-icon.png"),
-  iconSize: [38, 38],
-});
-
-const traffic = markers.reduce(
-  (acc, curr) => {
-    return curr.isLiveTraffic
-      ? { ...acc, liveTraffic: [...acc.liveTraffic, curr] }
-      : { ...acc, offline: [...acc.offline, curr] };
-  },
-  { liveTraffic: [], offline: [] }
-);
+import { Map } from "./components/map/Map";
+import { useState } from "react";
 
 function App() {
-  const [liveTraffic, setLiveTraffic] = useState(traffic.liveTraffic);
-  const [offline, setOffline] = useState(traffic.offline);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setLiveTraffic((prev) => (prev.length > 0 ? [] : traffic.liveTraffic));
-    }, 1000);
-
-    return () => clearTimeout(intervalId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [traffic.liveTraffic]);
+  const [currentTab, setCurrentTab] = useState("map");
 
   return (
-    <div className="marker-container">
-      <MapContainer center={[48.8566, 2.3522]} zoom={14}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <MarkerGroupCluster>
-          {[...offline, ...liveTraffic].map((location, i) => (
-            <CircleMarker
-              key={i}
-              center={[location.latitude, location.longitude]}
-              radius={8}
-              color="blue"
-            >
-              <Popup>{location.popUp}</Popup>
-            </CircleMarker>
-          ))}
-        </MarkerGroupCluster>
-      </MapContainer>
+    <div>
+      <div className="tabs-container">
+        <buton onClick={() => setCurrentTab("map")} className={`tab-button ${currentTab === "map" && "active-tab"}`}>Map</buton>
+        <buton onClick={() => setCurrentTab("sales")} className={`tab-button ${currentTab === "sales" && "active-tab"}`}>Sales</buton>
+      </div>
+      <Map />
     </div>
   );
 }
