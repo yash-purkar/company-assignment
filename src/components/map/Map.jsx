6334144458f5c52b7f745b977580403929372e14
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-02-25 01:41:53 +0530

    feat: Added marker details

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -2,98 +2,20 @@ import React from "react";
 import "leaflet/dist/leaflet.css";
 import { CircleMarker, MapContainer, Popup, TileLayer } from "react-leaflet";
 import { useEffect, useState } from "react";
-// Locations on the map
-const locations = [
-  {
-    latitude: 16.0,
-    longitude: 80.0,
-    popUp: "Andhra Pradesh",
-    isLiveTraffic: true,
-  },
-  {
-    latitude: 28.0,
-    longitude: 95.0,
-    popUp: "Arunachal Pradesh",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 26.2006043,
-    longitude: 92.9375739,
-    popUp: "Asam",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 25.11,
-    longitude: 85.32,
-    popUp: "Bihar",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 21.3,
-    longitude: 82.0,
-    popUp: "Chhattisgarh",
-    isLiveTraffic: true,
-  },
-  {
-    latitude: 28.38,
-    longitude: 72.12,
-    popUp: "Goa",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 23.0,
-    longitude: 72.0,
-    popUp: "Gujarat",
-    isLiveTraffic: true,
-  },
-  {
-    latitude: 30.3,
-    longitude: 74.6,
-    popUp: "Haryana",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 25.3,
-    longitude: 91.0,
-    popUp: "Meghalaya",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 26.0,
-    longitude: 94.2,
-    popUp: "Orissa",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 30.4,
-    longitude: 75.5,
-    popUp: "Punjab",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 27.0,
-    longitude: 74.0,
-    popUp: "Rajasthan",
-    isLiveTraffic: false,
-  },
-  {
-    latitude: 20,
-    longitude: 76,
-    popUp: "Maharashtra",
-    isLiveTraffic: false,
-  },
-];
-
-const traffic = locations.reduce(
-  (acc, curr) => {
-    return curr.isLiveTraffic
-      ? { ...acc, liveTraffic: [...acc.liveTraffic, curr] }
-      : { ...acc, offline: [...acc.offline, curr] };
-  },
-  { liveTraffic: [], offline: [] }
-);
+import { useGetTrafficData } from "./hooks/useGetTrafficData";
 
 export const Map = () => {
+  const locations = useGetTrafficData();
+
+  // To get liveTraffic data
+  const traffic = locations.reduce(
+    (acc, curr) => {
+      return curr.isLiveTraffic
+        ? { ...acc, liveTraffic: [...acc.liveTraffic, curr] }
+        : { ...acc, offline: [...acc.offline, curr] };
+    },
+    { liveTraffic: [], offline: [] }
+  );
   const [liveTraffic, setLiveTraffic] = useState(traffic.liveTraffic);
 
   // To blink the traffic locations
@@ -107,13 +29,12 @@ export const Map = () => {
   }, [traffic.liveTraffic]);
 
   return (
-    <div className="marker-container">
+      <div className="marker-container">
       <MapContainer center={[20, 78]} zoom={5}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* <MarkerGroupCluster> */}
         {[...traffic.offline, ...liveTraffic].map((location, i) => (
           <CircleMarker
             key={i}
@@ -124,8 +45,11 @@ export const Map = () => {
             <Popup>{location.popUp}</Popup>
           </CircleMarker>
         ))}
-        {/* </MarkerGroupCluster> */}
       </MapContainer>
+    <div className="customer-density-text">
+      <span></span>
+      <p>Customer density</p>
+    </div>
     </div>
   );
 };
